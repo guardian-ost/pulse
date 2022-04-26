9abdeda6c5be6f9d06736d7bcd900ae583a3d447
guardian.ost <guardian.ost@protonmail.com>
2022-04-26 09:55:00 +0300

    adding mailer

--- src/js/script.js
+++ src/js/script.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             });
-        });   
+        });
     }
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
@@ -37,18 +37,74 @@ $(document).ready(function () {
 
 
     //модальные окна
-    $('[data-modal=consultation]').on('click', function() {
+    $('[data-modal=consultation]').on('click', function () {
         $('.overlay, #consultation').fadeIn();
     });
 
-    $('.modal__close').on('click', function(){
+    $('.modal__close').on('click', function () {
         $('.overlay, #consultation, #order, #thanks').fadeOut();
     });
 
-    $('.button_mini').each(function(i){
-        $(this).on('click', function() {
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
             $('#order .modal__description').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn();
         })
     });
+    
+    function validateForm(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                  required: "Please enter your name",
+                  minlength: jQuery.validator.format("At least {0} characters required!")
+                },
+                phone: {
+                    required: "Please enter your phone"
+                },
+                email:{
+                    required: "Please enter your email",
+                    email: "You email is incorrect"
+                }
+                
+            }
+        });
+    }
+    validateForm($('#consultation form'));
+    validateForm($('#order form'));
+    validateForm($('#consultation-form'));
+
+    $('input[name=phone').mask("+38(999) 999-99-99");
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        if (!$(this).valid()) {
+            return;
+        }
+
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+            
+        });
+        return false;
+    })
 });
